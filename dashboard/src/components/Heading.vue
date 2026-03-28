c8a024be1c4e96c1b8340dcd430c76aa7e3e6003
Chen Yang <chen.yang@apexphere.co.nz>
2026-03-29 08:15:13 +1300

    revert: remove Vue 2 dashboard source — running dashboard is Nuxt 3 from a different source

--- dashboard/src/components/Heading.vue
+++
@@ -1,5 +0,0 @@
-<template>
-  <h3 class="text-2xl mb-4">
-    <slot />
-  </h3>
-</template>
--- dashboard/src/plugins/notyf.js
+++
@@ -1,9 +0,0 @@
-import { Notyf } from 'notyf'
-
-export default {
-  install: (app, settings) => {
-    settings.position.x = 'left'
-    app.config.globalProperties.notyf = new Notyf(settings)
-    app.notyf = app.config.globalProperties.notyf
-  }
-}
